1744fd801af83acb7167b52cec2a82123edc548a
hi-sushanta <iamchi@skiff.com>
2024-05-16 21:21:42 +0530

    damtrade app folder is uploaded

---
+++ damtrade/android/app/src/main/kotlin/com/example/damtrade/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.damtrade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ damtrade/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(), // Replace with your actual class name
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+        
+
+          // Option 2: Setting Image height to full (with aspect ratio)
+          Image.asset(
+            'assets/sushanta.jpeg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity
+          ),
+          // Container for buttons with positioning
+          Positioned(
+            left: 30.0,
+            bottom: 30.0, // Adjust spacing as needed
+            width: 100,
+            height:55.0,
+            
+            child: 
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle button 1 press
+                  },
+                  child: const Text(
+                    'Skip',
+                    style: TextStyle(color:Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 16)
+                    ),
+                   style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 234, 234, 234), // Set button color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                )
+              ),
+                  
+                ),
+             ), // Add spacing between buttons
+             Positioned(
+              right:30.0,
+              bottom:30.0,
+              width: 120.0,
+              height: 55.0,
+              child:
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle button 2 press
+                  },
+                  child: Text(
+                    'Next',
+                    style: TextStyle(color: Color.fromARGB(225, 255, 255, 255),
+                    fontSize: 16)
+                    ),
+                  style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 0, 0, 0), // Set button color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)
+                  ),
+              ),
+                  
+                ),
+
+            ),
+        ],  
+      ),
+    );
+  }
+}
