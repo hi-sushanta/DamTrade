cbef77e084650ee6c1b47792e6597a28456ba27a
hi-sushanta <iamchi@skiff.com>
2024-05-19 01:51:12 +0530

    Now I update my code for doing sighing and signup option

---
+++ damtrade/lib/pages/auth_gate.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class AuthGate extends StatelessWidget {
+ const AuthGate({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+   return StreamBuilder<User?>(
+     stream: FirebaseAuth.instance.authStateChanges(),
+     builder: (context, snapshot) {
+       if (!snapshot.hasData) {
+         return SignInScreen(
+           providers: [
+             EmailAuthProvider(),
+             GoogleProvider(clientId: "467721621108-o933an06qskqs8v5hse94ct5buqji8gh.apps.googleusercontent.com"),  // new
+           ],
+           headerBuilder: (context, constraints, shrinkOffset) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('flutterfire_300x.png'),
+               ),
+             );
+           },
+           subtitleBuilder: (context, action) {
+             return Padding(
+               padding: const EdgeInsets.symmetric(vertical: 8.0),
+               child: action == AuthAction.signIn
+                   ? const Text('Welcome to FlutterFire, please sign in!')
+                   : const Text('Welcome to Flutterfire, please sign up!'),
+             );
+           },
+           footerBuilder: (context, action) {
+             return const Padding(
+               padding: EdgeInsets.only(top: 16),
+               child: Text(
+                 'By signing in, you agree to our terms and conditions.',
+                 style: TextStyle(color: Colors.grey),
+               ),
+             );
+           },
+           sideBuilder: (context, shrinkOffset) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('flutterfire_300x.png'),
+               ),
+             );
+           },
+         );
+       }
+
+       return const Home();
+     },
+   );
+ }
+}
--- damtrade/lib/pages/home.dart
+++ damtrade/lib/pages/home.dart
@@ -1,19 +1,11 @@
-import 'package:damtrade/firebase_options.dart';
+
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'pages/auth_gate.dart';
 
-void main() async {
-  // calling of runApp
- WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(
-   options: DefaultFirebaseOptions.currentPlatform,
- );
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(const Home());
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -53,8 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: ElevatedButton(
                       onPressed: () {
                     // Handle button 1 press
-                     Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => AuthGate()));
+                     
 
                     },
                   child: const Text(
