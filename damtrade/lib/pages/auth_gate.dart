356e6c9d020809714cee03ff15ca139dc2727929
hi-sushanta <iamchi@skiff.com>
2024-07-17 23:53:49 +0530

    Add back ground color and replace flutter app to dam-trade

--- damtrade/lib/pages/auth_gate.dart
+++ damtrade/lib/pages/auth_gate.dart
@@ -13,46 +13,51 @@ class AuthGate extends StatelessWidget {
      stream: FirebaseAuth.instance.authStateChanges(),
      builder: (context, snapshot) {
        if (!snapshot.hasData) {
-         return SignInScreen(
-           providers: [
-             EmailAuthProvider(),
-             GoogleProvider(clientId: "467721621108-o933an06qskqs8v5hse94ct5buqji8gh.apps.googleusercontent.com"),  // new
-           ],
-           headerBuilder: (context, constraints, shrinkOffset) {
-             return Padding(
-               padding: const EdgeInsets.all(20),
-               child: AspectRatio(
-                 aspectRatio: 1,
-                 child: Image.asset('flutterfire_300x.png'),
-               ),
-             );
-           },
-           subtitleBuilder: (context, action) {
-             return Padding(
-               padding: const EdgeInsets.symmetric(vertical: 8.0),
-               child: action == AuthAction.signIn
-                   ? const Text('Welcome to FlutterFire, please sign in!')
-                   : const Text('Welcome to Flutterfire, please sign up!'),
-             );
-           },
-           footerBuilder: (context, action) {
-             return const Padding(
-               padding: EdgeInsets.only(top: 16),
-               child: Text(
-                 'By signing in, you agree to our terms and conditions.',
-                 style: TextStyle(color: Colors.grey),
-               ),
-             );
-           },
-           sideBuilder: (context, shrinkOffset) {
-             return Padding(
-               padding: const EdgeInsets.all(20),
-               child: AspectRatio(
-                 aspectRatio: 1,
-                 child: Image.asset('flutterfire_300x.png'),
-               ),
-             );
-           },
+         return Theme(
+           data: Theme.of(context).copyWith(
+             scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255), // Set your desired background color here
+           ),
+           child: SignInScreen(
+             providers: [
+               EmailAuthProvider(),
+               GoogleProvider(clientId: "467721621108-o933an06qskqs8v5hse94ct5buqji8gh.apps.googleusercontent.com"),
+             ],
+             headerBuilder: (context, constraints, shrinkOffset) {
+               return Padding(
+                 padding: const EdgeInsets.all(20),
+                 child: AspectRatio(
+                   aspectRatio: 1,
+                   child: Image.asset('assets/sushanta.jpeg'),
+                 ),
+               );
+             },
+             subtitleBuilder: (context, action) {
+               return Padding(
+                 padding: const EdgeInsets.symmetric(vertical: 8.0),
+                 child: action == AuthAction.signIn
+                     ? const Text('Welcome to Dam-Trade App, please sign in!')
+                     : const Text('Welcome to Dam-Trade App, please sign up!'),
+               );
+             },
+             footerBuilder: (context, action) {
+               return const Padding(
+                 padding: EdgeInsets.only(top: 16),
+                 child: Text(
+                   'By signing in, you agree to our terms and conditions.',
+                   style: TextStyle(color: Colors.grey),
+                 ),
+               );
+             },
+             sideBuilder: (context, shrinkOffset) {
+               return Padding(
+                 padding: const EdgeInsets.all(20),
+                 child: AspectRatio(
+                   aspectRatio: 1,
+                   child: Image.asset('flutterfire_300x.png'),
+                 ),
+               );
+             },
+           ),
          );
        }
        return const Home();
