d5fce7681f0f91219104f02eed6b5c884093fb74
hi-sushanta <hisushanta@proton.me>
2024-08-02 10:23:48 +0530

    Also update sideBuilder image

--- damtrade/lib/pages/auth_gate.dart
+++ damtrade/lib/pages/auth_gate.dart
@@ -53,7 +53,7 @@ class AuthGate extends StatelessWidget {
                  padding: const EdgeInsets.all(20),
                  child: AspectRatio(
                    aspectRatio: 1,
-                   child: Image.asset('flutterfire_300x.png'),
+                   child: Image.asset('assets/BullAndBear.jpg'),
                  ),
                );
              },
